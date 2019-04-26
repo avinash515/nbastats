a2d20aaf73a8c18ef030bff5882fc3134013b8fe
Avinash Gottumukkale <Avinash@Avinashs-MacBook-Pro.local>
2019-04-26 16:50:31 -0500

    basic database setup but has different node_modules files

--- backend/app.py
+++ backend/app.py
@@ -8,16 +8,30 @@ from flask import Flask, render_template
 application = Flask(__name__)
 app = application
 
-def get_db_creds():
-    db = os.environ.get("DB", None) or os.environ.get("database", None)
-    username = os.environ.get("USER", None) or os.environ.get("username", None)
-    password = os.environ.get("PASSWORD", None) or os.environ.get("password", None)
-    hostname = os.environ.get("HOST", None) or os.environ.get("dbhost", None)
-    return db, username, password, hostname
+def test_function():
+    server = 'nbastats.database.windows.net'
+    database = 'NbaStats'
+    username = 'user'
+    password = 'Preston123avinash'
+    driver='{ODBC Driver 13 for SQL Server}'
+    cnxn = pyodbc.connect('DRIVER='+driver+';SERVER='+server+';PORT=1433;DATABASE='+database+';UID='+username+';PWD='+ password)
+    cursor = cnxn.cursor()
+    cursor.execute("SELECT Player, Pts, Ast, Trb, Stl from nbastats")
+    row = cursor.fetchone()
+    while row:
+        print (str(row))
+        row = cursor.fetchone()
 
 @app.route("/")
 def test():
   return "test"
+
+try:
+    print("Before test_function global")
+    test_function()
+    print("After test_function global")
+except Exception as exp:
+    print("Got exception %s" % exp)
 
 if __name__ == "__main__":
     app.debug = True
